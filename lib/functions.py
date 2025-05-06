2d9209ade3893a1e9a0f951cefb6570a05ed7225
Boniface <bm715452@gmail.com>
2025-05-06 04:18:20 +0300

    passing functions

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,27 @@
 
 def greet_programmer():
     pass
+    print(f"Hello, programmer!")
+greet_programmer()
 
-def greet(name):
+# Hello, name!
+def greet(name="Naureen"):
     pass
+    print(f"Hello, {name}!")
+greet()
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default()
 
-def add(num1, num2):
+def add(num1=45, num2=55):
     pass
+    return num1 + num2
+add()
 
-def halve(number):
+def halve(number=80.0):
     pass
+    return number / 2
+halve()
+
